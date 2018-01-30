7503c843c366471976c330ca88aa2e756d9744cf
Dan <dshuhler@gmail.com>
2018-01-30 18:31:37 -0500

    Simplify and add unit tests

--- codility/iterations/binary_gap.py
+++ codility/iterations/binary_gap.py
@@ -1,19 +1,15 @@
-
-def decimal_to_binary(dec_num):
-    result = ''
-    while dec_num > 0:
-        result = result + str(dec_num % 2)
-        dec_num = dec_num // 2
-    return result[::-1]
+import unittest
 
 
 def solution(N):
-    binary_n = decimal_to_binary(N)
+
+    # convert to binary string
+    binary_s = "{0:b}".format(N)
 
     this_length = 0
     max_length = 0
 
-    for c in binary_n:
+    for c in binary_s:
         if c == '1':
             max_length = max(max_length, this_length)
             this_length = 0
@@ -23,7 +19,20 @@ def solution(N):
     return max_length
 
 
-print(solution(9))
-print(solution(529))
-print(solution(20))
-print(solution(15))
+class TestBinaryGap(unittest.TestCase):
+
+    def test_samples(self):
+        self.assertEqual(solution(9), 2)
+        self.assertEqual(solution(529), 4)
+        self.assertEqual(solution(1041), 5)
+
+    def test_no_gap(self):
+        self.assertEqual(solution(6), 0)
+        self.assertEqual(solution(15), 0)
+
+    def test_zero(self):
+        self.assertEqual(solution(0), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
