ade6af765cb66ed979386d8546b14e36c4d0e537
Dan <dshuhler@gmail.com>
2018-02-01 06:20:04 -0500

    Add tests

--- codility/3_time_complexity/frog_jump.py
+++ codility/3_time_complexity/frog_jump.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def solution(X, Y, D):
     distance = Y - X
     jumps = (distance // D)
@@ -6,8 +9,20 @@ def solution(X, Y, D):
     return jumps
 
 
-print(solution(10, 85, 30), 3)
-print(solution(0, 0, 30), 0)
-print(solution(10, 11, 100000), 1)
-print(solution(10, 90, 30), 3)
+class TestPassingCars(unittest.TestCase):
 
+    def test_sample(self):
+        self.assertEqual(3, solution(10, 85, 30))
+
+    def test_no_distance(self):
+        self.assertEqual(0, solution(10, 10, 1))
+
+    def test_max_jump(self):
+        self.assertEqual(1, solution(1, 2, 1000000000))
+
+    def test_max_num_jumps(self):
+        self.assertEqual(1000000000, solution(0, 1000000000, 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
