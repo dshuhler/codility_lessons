f53ff73cfb00d025fbe2d49fd79f6a7a2dee7d70
Dan <dshuhler@gmail.com>
2018-02-06 20:42:45 -0500

    Add tests

--- codility/4_counting_elements/frog_river_one.py
+++ codility/4_counting_elements/frog_river_one.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def solution(X, A):
     leaf_coverage = [False] * X
     frog_position = -1
@@ -15,7 +18,19 @@ def solution(X, A):
     return -1
 
 
-print(solution(5, [1, 3, 2, 4, 3, 3, 5, 2]))
-print(solution(5, [1, 3, 2, 4, 3, 3, 4, 2]))
-print(solution(5, [5, 5, 4, 3, 2, 2, 2, 1]))
+class TestPassingCars(unittest.TestCase):
+
+    def test_sample(self):
+        self.assertEqual(6, solution(5, [1, 3, 2, 4, 3, 3, 5, 2]))
+
+    def test_not_able_to_cross(self):
+        self.assertEqual(-1, solution(5, [1, 3, 2, 4, 3, 3, 4, 2]))
+
+    def test_big_river(self):
+        self.assertEqual(100000, solution(100001, list(range(0, 100001))))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 
