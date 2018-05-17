9d2920a1cdbc68da278f4db92ab92b3118d3fec3
Dan <dshuhler@gmail.com>
2018-05-17 17:04:12 -0400

    Refactor to not use prefix sums as it made life harder :(

--- codility/5_prefix_sums/min_avg_two_slice.py
+++ codility/5_prefix_sums/min_avg_two_slice.py
@@ -5,46 +5,28 @@ import unittest
 # the big slice.
 #
 # knowing this allows us to move across the array checking all the slices 2 - 3 in length (linear time). You don't even
-# have to use prefix sums, but I did anyway :)
+# have to use prefix sums.
 
 
-def prefix_sums(vals):
-    result = [0] * len(vals)
-    current_sum = 0
-    for i, val in enumerate(vals):
-        current_sum += val
-        result[i] = current_sum
-    return result
+def slice_avg(vals, start_index, length):
+    total = sum(vals[start_index:start_index + length])
+    return total / length
 
 
 def solution(A):
 
-    psums = prefix_sums(A)
+    min_index = 0
+    min_value = 10001
 
-    smallest_start_index = 0
-    smallest_avg = 0
+    for idx in range(0, len(A) - 1):
+        if slice_avg(A, idx, 2) < min_value:
+            min_index = idx
+            min_value = slice_avg(A, idx, 2)
+        if idx < len(A) - 2 and slice_avg(A, idx, 3) < min_value:
+            min_index = idx
+            min_value = slice_avg(A, idx, 3)
 
-    for i in range(len(A) - 1):
-
-        if i == 0:
-            two_slice_avg = psums[1] / 2
-            three_slice_avg = psums[2] / 3
-            if two_slice_avg < three_slice_avg:
-                smallest_avg = two_slice_avg
-            else:
-                smallest_avg = three_slice_avg
-        else:
-            two_slice_avg = (psums[i + 1] - psums[i - 1]) / 2
-            if two_slice_avg < smallest_avg:
-                smallest_start_index = i
-                smallest_avg = two_slice_avg
-            if i < len(A) - 2:
-                three_slice_avg = (psums[i + 2] - psums[i - 1]) / 3
-                if three_slice_avg < smallest_avg:
-                    smallest_start_index = i
-                    smallest_avg = three_slice_avg
-
-    return smallest_start_index
+    return min_index
 
 
 class TestMinAvgTwoSlice(unittest.TestCase):
@@ -61,3 +43,8 @@ class TestMinAvgTwoSlice(unittest.TestCase):
     def test_same(self):
         self.assertEqual(0, solution([5, 5, 5, 5, 5]))
 
+    def test_two_elements(self):
+        self.assertEqual(0, solution([5, 5]))
+
+    def test_four_elements(self):
+        self.assertEqual(2, solution([1, 5, 1, 1]))
