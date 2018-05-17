251f9f843da21d2f49f79935b7ea48f8d33188a8
Dan <dshuhler@gmail.com>
2018-05-17 13:28:23 -0400

    Use unit tests

--- codility/4_counting_elements/perm_check.py
+++ codility/4_counting_elements/perm_check.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def solution(A):
     occurances = [0] * len(A)
 
@@ -11,7 +14,29 @@ def solution(A):
     return 1
 
 
-print(solution([4, 1, 2, 3]))
-print(solution([4, 1, 3]))
-print(solution([4, 10, 3]))
-print(solution([4, 10, 3, 1, 2, 5, 6, 7, 9, 8]))
+class TestPermCheck(unittest.TestCase):
+
+    def test_true_sample(self):
+        self.assertEqual(1, solution([4, 1, 2, 3]))
+
+    def test_false_sample(self):
+        self.assertEqual(0, solution([4, 1, 3]))
+
+    def test_multiple_missing(self):
+        self.assertEqual(0, solution([4, 10, 3]))
+
+    def test_one_to_ten_randomized(self):
+        self.assertEqual(1, solution([4, 10, 3, 1, 2, 5, 6, 7, 9, 8]))
+
+    def test_big_with_missing(self):
+        big_list = list(range(1, 100000))
+        big_list.remove(400)
+        self.assertEqual(0, solution(big_list))
+
+    def test_big_reverse_order(self):
+        big_list = list(range(100000, 0, -1))
+        self.assertEqual(1, solution(big_list))
+
+
+if __name__ == '__main__':
+    unittest.main()
