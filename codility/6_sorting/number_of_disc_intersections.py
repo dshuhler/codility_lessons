4703ee061b28357def54fa5eae8b906188205571
Dan <dshuhler@gmail.com>
2018-05-18 16:44:48 -0400

    fix sorting

--- codility/6_sorting/number_of_disc_intersections.py
+++ codility/6_sorting/number_of_disc_intersections.py
@@ -4,16 +4,16 @@ import unittest
 def solution(A):
     disc_edges = []
     for i, radius in enumerate(A):
-        disc_edges.append((i - radius, "START"))
-        disc_edges.append((i + radius, "END"))
+        disc_edges.append((i - radius, True))
+        disc_edges.append((i + radius, False))
 
     disc_edges.sort(key=lambda x: (x[0], not x[1]))
 
     num_intersections = 0
     active_circles = 0
 
-    for _, edge_type in disc_edges:
-        if edge_type == "START":
+    for _, is_edge_start in disc_edges:
+        if is_edge_start:
             num_intersections += active_circles
             active_circles += 1
         else:
@@ -29,6 +29,9 @@ class TestNumberOfDiscIntersections(unittest.TestCase):
 
     def test_sample(self):
         self.assertEqual(11, solution([1, 5, 2, 1, 4, 0]))
+
+    def test_three(self):
+        self.assertEqual(3, solution([1, 1, 1]))
 
 
 if __name__ == '__main__':
